d6b197041a0b1e5de126bca63caea323e628c596
Naveen Vijay <naveenvijay@naveens-mac.local>
2018-05-24 00:49:07 +0530

    collection

---
+++ Entertainment/CollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewCell.swift
+//  Entertainment
+//
+//  Created by Naveen Vijay on 23/05/18.
+//  Copyright © 2018 Naveen Vijay. All rights reserved.
+//
+
+import UIKit
+
+class  CollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var imageDisplay: UIImageView!
+    @IBOutlet weak var labelText: UILabel!
+    
+    func funcDisplayCollection(image: UIImage, title: String)
+    {
+        imageDisplay.image = image
+        labelText.text = title
+    }
+}
